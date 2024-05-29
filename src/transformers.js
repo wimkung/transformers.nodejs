876130a7c38d5342c140d8cfd176a3393f8ecfb8
wimkung <wimkung@gmail.com>
2024-05-29 21:01:26 +0700

    build: remove image and audio utils

--- src/transformers.js
+++ src/transformers.js
@@ -1,13 +1,13 @@
 /**
  * @file Entry point for the Transformers.js library. Only the exports from this file
  * are available to the end user, and are grouped as follows:
- * 
+ *
  * 1. [Pipelines](./pipelines)
  * 2. [Environment variables](./env)
  * 3. [Models](./models)
  * 4. [Tokenizers](./tokenizers)
  * 5. [Processors](./processors)
- * 
+ *
  * @module transformers
  */
 
@@ -18,7 +18,5 @@ export * from './tokenizers.js';
 export * from './processors.js';
 export * from './configs.js';
 
-export * from './utils/audio.js';
-export * from './utils/image.js';
 export * from './utils/tensor.js';
 export * from './utils/maths.js';
